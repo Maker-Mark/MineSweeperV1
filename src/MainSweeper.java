714b280e0c587f3c41a50744fff524e274bbc874
Mark <markgoldstein12@gmail.com>
2018-11-16 00:16:59 -0500

    added move option

--- src/MainSweeper.java
+++ src/MainSweeper.java
@@ -6,6 +6,7 @@ public class MainSweeper {
 	public static void main(String[] args) {
 
 		int rows, cols;
+		boolean done = false;
 		Scanner sc = new Scanner(System.in);	
 
 
@@ -27,22 +28,60 @@ public class MainSweeper {
 
 		System.out.println("how many mines do you want to make?");
 		int mines = sc.nextInt();
-	
+
 		setMines(mines, board);
-		
-		
-		
+
 		for (int iCol = 0; iCol < cols; iCol++) {
 			System.out.println();
 			for (int jRow = 0; jRow < rows; jRow++) {
 				if (board[iCol][jRow] == false) {
 					System.out.print(1);
-				}else {
+				} else {
 					System.out.print(0);
 				}
 
 			}
 
+		}
+		
+		
+	
+		while(!done) {
+
+			System.out.println("What move do you want to make? (c for click and f for flag)");
+
+			switch (sc.next().charAt(0)) {
+			case'c':
+			case'C':
+				click(sc, board);
+				done = check(board);
+				break;
+
+			case'f':
+			case'F':
+				flag(sc, board);
+				done = check(board);
+				break;
+
+			default:
+
+				System.out.println("Error: Invalid input");
+				break;
+			}
+
+
+
+			for (int iCol = 0; iCol < cols; iCol++) {
+				System.out.println();
+				for (int jRow = 0; jRow < rows; jRow++) {
+					if (board[iCol][jRow] == false) {
+						System.out.print(1);
+					} else {
+						System.out.print(0);
+					}
+
+				}
+			}
 		}
 	}
 
@@ -57,4 +96,8 @@ public class MainSweeper {
 			}
 		}
 	}
+
+
+
+
 }
