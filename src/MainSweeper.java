c78c054097834214276a7397ab4bfa3c8cdc99f0
Mark <markgoldstein12@gmail.com>
2018-11-15 23:44:38 -0500

    base main

--- src/MainSweeper.java
+++ src/MainSweeper.java
@@ -1,10 +1,50 @@
+import java.util.*;
 
 public class MainSweeper {
-	
-	
-	public static void main(String[] args) {
-		System.out.println("here is what I want you to");
-	
-	}
 
+
+	public static void main(String[] args) {
+
+		int rows, cols;
+		Scanner sc = new Scanner(System.in);	
+
+
+		System.out.println("How many rows in the board?");
+		rows = sc.nextInt();
+		System.out.println("How many cols in the board?");
+		cols = sc.nextInt();	
+		System.out.println("Ok, cool your board will be " + rows + " rows and " + cols + "columns!");
+
+
+
+		boolean [][] board = new boolean[cols][rows] ;
+		for (int i = 0; i < cols; i++) {
+			for (int j = 0; j < rows; j++) {
+				board[i][j] = false;
+
+			}
+		}
+
+		System.out.println("how many mines do you want to make?");
+		int mines = sc.nextInt();
+
+//		setMines(mines, board);
+
+
+		for (int i = 0; i < cols; i++) {
+			System.out.println();
+			for (int j = 0; j < rows; j++) {
+				if (board[i][j] == false) {
+					System.out.print(1);
+				}else {
+				System.out.print(0);
+				}
+				
+			}
+
+
+
+
+		}
+	}
 }
